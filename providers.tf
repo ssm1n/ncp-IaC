fa9f3f5ef3c5247ddfda1dd791dc2754a7b36d4b
USER <user@AL02274322.local>
2025-12-08 16:06:05 +0900

    feat: provider 설정

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    ncloud = {
+      source  = "NavercloudPlatform/ncloud"
+      version = ">= 3.2.0"
+    }
+  }
+}
+
+provider "ncloud" {
+  region      = var.region
+  site        = var.site
+  support_vpc = true
+
+  access_key = var.ncloud_access_key
+  secret_key = var.ncloud_secret_key
+}
+
